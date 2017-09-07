f6a44ed90b67d57cac886d66f9269b137493fc36
powi442 <k08ip01@gmail.com>
2017-09-07 08:59:48 -0700

    everything is working

--- config/orm.js
+++ config/orm.js
@@ -69,7 +69,25 @@ create: function(table, cols, vals, cb) {
       }
       cb(result);
     });
-  }
+  },
+  // An example of objColVals would be {name: panther, sleepy: true}
+  update: function(table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+}
 }
 // Export the orm object for the model ().
 module.exports = orm;
