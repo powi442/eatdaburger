6044a329edbe26dcb6e1274daae6e765f7dda407
powi442 <k08ip01@gmail.com>
2017-09-07 09:42:18 -0700

    changed port

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,10 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  port: 3306,
+  port: var port = process.env.PORT || 3500;,
   host: "localhost",
   user: "root",
-  password: "",
+  password: "spanish",
   database: "hamburger_db"
 });
 
